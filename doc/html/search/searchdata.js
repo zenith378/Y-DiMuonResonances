d96c24ac12fd461602b732f18a813b63b538d56f
Matilde Carminati <matilde@MacBook-Pro-di-Matilde.local>
2022-12-05 10:47:27 +0100

    nuovo ramo per la documentazione

---
+++ doc/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "bfgl",
+  1: "f",
+  2: "bfgl"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Functions"
+};
+
