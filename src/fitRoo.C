1d9372ba633c510a195e223cb4f45441836f7c5b
Giulio Cordova <zenith378@MacBook-Pro-di-Giulio.local>
2022-12-07 23:39:56 +0100

    implemented choice of function, suppressed warnings, fit values printed nicely

--- src/fitRoo.C
+++ src/fitRoo.C
@@ -4,11 +4,13 @@
 #include "RooPolynomial.h"
 #include "RooBreitWigner.h"
 #include "RooAddPdf.h"
+#include "RooAbsPdf.h"
 #include "RooGlobalFunc.h"
 #include "TCanvas.h"
 #include "TAxis.h"
 #include "RooPlot.h"
 #include "TH1.h"
+#include "RooFitResult.h"
 #include "RooDataHist.h"
 #include "RooHist.h"
 #include "TRootCanvas.h"
@@ -17,27 +19,15 @@
 #include <string.h>
 
 using namespace RooFit;
-/*
-RooAddPdf model_gaus(RooPolynomial bakg, RooRealVar x,RooRealVar mean1, RooRealVar mean2,RooRealVar mean3,RooRealVar sigma1,RooRealVar sigma2,RooRealVar sigma3){
-    std::cout << "Using gaus" << std::endl;
-    RooGaussian *sig_gaus1("sig1_gaus", "Signal component 1", x, mean1, sigma1);
-    RooGaussian *sig_gaus2("sig2_gaus", "Signal component 2", x, mean2, sigma2);
-    RooGaussian *sig_gaus3("sig3_gaus", "Signal component 3", x, mean3, sigma3);
-    RooAddPdf* model_gaus("model_gaus", "model", RooArgList(*sig_gaus1, *sig_gaus2, *sig_gaus3, bkg), RooArgList(fsig1, fsig2, fsig3), kTRUE);
-
-
-}
-*/
 /**************************************************************
  \brief three peaks fit plus a linear background
- 
- 
+
+
  *******************************************************************************/
 void fitRoo(TH1 *hh, int functype)
 {
     // Set up   component   pdfs
     // ---------------------------------------
-    
 
     // Declare observable x
     RooRealVar x("x", "m_{#mu^{+}#mu^{-}} (GeV/c^{2})", 8.5, 11.5);
@@ -47,9 +37,9 @@ void fitRoo(TH1 *hh, int functype)
 
     // Build polynomial pdf
     RooRealVar a0("a0", "a0", 3101, 0, 10000);
-    RooRealVar a1("a1", "a1", -140.3, -300., 300.);
-    RooRealVar a2("a2", "a2", 0, -300., 300.);
-    RooPolynomial bkg("bkg", "Background", x, RooArgSet(a0, a1,a2));
+    RooRealVar a1("a1", "a1", -140.3, -300., 0.);
+    RooRealVar a2("a2", "a2", 0, -30., 30.);
+    RooPolynomial bkg("bkg", "Background", x, RooArgSet(a0, a1, a2));
 
     // Create three Gaussian PDFs and their parameters
     RooRealVar mean1("mean1", "mean of gaussians", 9.45, 3.3, 9.6);
@@ -61,97 +51,94 @@ void fitRoo(TH1 *hh, int functype)
 
     // Add signal and background
 
-    RooRealVar fsig1("fsig1", "signal1", 0.1, 0., 1.);
-    RooRealVar fsig2("fsig2", "signal2", 0.1, 0., 1.);
-    RooRealVar fsig3("fsig3", "signal3", 0.1, 0., 1.);
+    RooRealVar fsig1("fsig1", "signal1", 0.6, 0., 1.);
+    RooRealVar fsig2("fsig2", "signal2", 0.3, 0., 1.);
+    RooRealVar fsig3("fsig3", "signal3", 0.2, 0., 1.);
 
     RooPlot *xframe = x.frame(Title("Y Resonances Fit"));
+
+    RooAbsPdf *sig1;
+    RooAbsPdf *sig2;
+    RooAbsPdf *sig3;
 
     switch (functype)
     {
 
-    case 0: {
+    case 0:
+    default:
+    {
         std::cout << "Using Breit-Wigner" << std::endl;
-        RooBreitWigner sig_bw1("sig1_bw1", "Signal component 1", x, mean1, sigma1);
-        RooBreitWigner sig_bw2("sig2_bw2", "Signal component 2", x, mean2, sigma2);
-        RooBreitWigner sig_bw3("sig3_bw3", "Signal component 3", x, mean3, sigma3);
-        RooAddPdf model_bw("model_bw", "model", RooArgList(sig_bw1, sig_bw2, sig_bw3, bkg), RooArgList(fsig1, fsig2, fsig3), kTRUE);
 
-        //  Fit model to data
-        model_bw.fitTo(rh);
+        sig1 = new RooBreitWigner("sig1", "Signal component 1", x, mean1, sigma1);
 
-        // Draw options
-        // ---------------------------------------
+        sig2 = new RooBreitWigner("sig2", "Signal component 2", x, mean2, sigma2);
 
-        // Plot data and PDF overlaid
-        rh.plotOn(xframe, MarkerStyle(6), MarkerSize(1));
+        sig3 = new RooBreitWigner("sig3", "Signal component 3", x, mean3, sigma3);
 
-        // Overlay the background component of model with a dashed line
-        model_bw.plotOn(xframe, Components(bkg), LineColor(kBlue), LineStyle(kDashed));
-        // Overlay the sig1 components of model with a dashed-dotted line
-        model_bw.plotOn(xframe, Components(RooArgSet(sig_bw1)), LineColor(kRed), LineStyle(8));
-        // Overlay the background+sig2 components of model with a long dashed line
-        model_bw.plotOn(xframe, Components(RooArgSet(sig_bw2)), LineColor(kGreen), LineStyle(9));
-
-        model_bw.plotOn(xframe, Components(RooArgSet(sig_bw3)), LineStyle(kDotted));
-
-        // Print structure of composite pdf
-        model_bw.Print("t"); // previous was t
-
-        model_bw.plotOn(xframe);
         break;
     }
-    case 1:{
+
+    case 1:
+    {
         std::cout << "Using gaus" << std::endl;
-        RooGaussian sig_gaus1("sig1_gaus", "Signal component 1", x, mean1, sigma1);
-        RooGaussian sig_gaus2("sig2_gaus", "Signal component 2", x, mean2, sigma2);
-        RooGaussian sig_gaus3("sig3_gaus", "Signal component 3", x, mean3, sigma3);
-
-        RooAddPdf model_gaus("model_gaus", "model", RooArgList(sig_gaus1, sig_gaus2, sig_gaus3, bkg), RooArgList(fsig1, fsig2, fsig3), kTRUE);
-        //  Fit model to data
-        model_gaus.fitTo(rh);
-
-        // Draw options
-        // ---------------------------------------
-
-        // Plot data and PDF overlaid
-        rh.plotOn(xframe, MarkerStyle(6), MarkerSize(1));
-
-        // Overlay the background component of model with a dashed line
-        model_gaus.plotOn(xframe, Components(bkg), LineColor(kBlue), LineStyle(kDashed));
-        // Overlay the sig1 components of model with a dashed-dotted line
-        model_gaus.plotOn(xframe, Components(RooArgSet(sig_gaus1)), LineColor(kRed), LineStyle(8));
-        // Overlay the background+sig2 components of model with a long dashed line
-        model_gaus.plotOn(xframe, Components(RooArgSet(sig_gaus2)), LineColor(kGreen), LineStyle(9));
-
-        model_gaus.plotOn(xframe, Components(RooArgSet(sig_gaus3)), LineStyle(kDotted));
-
-        // Print structure of composite pdf
-        model_gaus.Print("t"); // previous was t
-
-        model_gaus.plotOn(xframe);
-        break;
-    }
-    }
-
-    /*
-
-
-    if(strcmp(functype,"gaus")==0){
-    std::cout << "Using gaus" << std::endl;
         sig1 = new RooGaussian("sig1", "Signal component 1", x, mean1, sigma1);
         sig2 = new RooGaussian("sig2", "Signal component 2", x, mean2, sigma2);
         sig3 = new RooGaussian("sig3", "Signal component 3", x, mean3, sigma3);
-    } else if(strcmp(functype,"bw")==0) {
-        std::cout << "Using gaus" << std::endl;
-        sig1 = new RooBreitWigner("sig1", "Signal component 1", x, mean1, sigma1);
-        sig2 = new RooBreitWigner("sig2", "Signal component 2", x, mean2, sigma2);
-        sig3 = new RooBreitWigner("sig3", "Signal component 3", x, mean3, sigma3);
-    } else {
-        std::cout << "Function must be gaus or bw" << std::endl;
-        std::cout << "Using Breit-Wigner (default)" << std::endl;
+        break;
+        /*
+                RooAddPdf model_gaus("model_gaus", "model", RooArgList(sig_gaus1, sig_gaus2, sig_gaus3, bkg), RooArgList(fsig1, fsig2, fsig3), kTRUE);
+                //  Fit model to data
+                model_gaus.fitTo(rh);
+
+                // Draw options
+                // ---------------------------------------
+
+                // Plot data and PDF overlaid
+                rh.plotOn(xframe, MarkerStyle(6), MarkerSize(1));
+
+                // Overlay the background component of model with a dashed line
+                model_gaus.plotOn(xframe, Components(bkg), LineColor(kBlue), LineStyle(kDashed));
+                // Overlay the sig1 components of model with a dashed-dotted line
+                model_gaus.plotOn(xframe, Components(RooArgSet(sig_gaus1)), LineColor(kRed), LineStyle(8));
+                // Overlay the background+sig2 components of model with a long dashed line
+                model_gaus.plotOn(xframe, Components(RooArgSet(sig_gaus2)), LineColor(kGreen), LineStyle(9));
+
+                model_gaus.plotOn(xframe, Components(RooArgSet(sig_gaus3)), LineStyle(kDotted));
+
+                // Print structure of composite pdf
+                model_gaus.Print("t"); // previous was t
+
+                model_gaus.plotOn(xframe);
+                break;
+
+                */
     }
-    */
+    }
+
+    RooAddPdf model("model", "model", RooArgList(*sig1, *sig2, *sig3, bkg), RooArgList(fsig1, fsig2, fsig3), kTRUE);
+
+    //  Fit model to data
+    RooFitResult* fitResult= model.fitTo(rh, Verbose(false), PrintLevel(-1), Warnings(false), PrintEvalErrors(-1), Timer(true), Save());
+
+    // Draw options
+    // ---------------------------------------
+
+    // Plot data and PDF overlaid
+    rh.plotOn(xframe, MarkerStyle(6), MarkerSize(1));
+
+    // Overlay the background component of model with a dashed line
+    model.plotOn(xframe, Components(bkg), LineColor(kBlue), LineStyle(kDashed));
+    // Overlay the sig1 components of model with a dashed-dotted line
+    model.plotOn(xframe, Components(RooArgSet(*sig1)), LineColor(kRed), LineStyle(8));
+    // Overlay the background+sig2 components of model with a long dashed line
+    model.plotOn(xframe, Components(RooArgSet(*sig2)), LineColor(kGreen), LineStyle(9));
+
+    model.plotOn(xframe, Components(RooArgSet(*sig3)), LineStyle(kDotted));
+
+    // Print structure of composite pdf
+    fitResult->Print("v"); // previous was t
+
+    model.plotOn(xframe);
 
     // Define model and fit
     // ---------------------------------------
@@ -173,8 +160,8 @@ void fitRoo(TH1 *hh, int functype)
 
     xframe->GetYaxis()->SetTitleOffset(1.5);
     xframe->GetXaxis()->SetTitleSize(0);
-    //xframe->GetXaxis()->SetLabelSize(0);
-    // xframe->GetXaxis()->SetTitleOffset(999);
+    // xframe->GetXaxis()->SetLabelSize(0);
+    //  xframe->GetXaxis()->SetTitleOffset(999);
     xframe->SetMinimum(0.001);
 
     xframe->Draw();
--- src/main.C
+++ src/main.C
@@ -1,22 +1,19 @@
 /**************************************************************
-* \file main.C
-* \brief main code
-*
-* dalla cartella Y-Dimuon
-*
-* cmake -S . -B ./build
-* questo dice dove buildare il codice da quale sorgente (e crea il makefile)
-*
-* make -C build
-* con questo si compila propriamente e si crea la libreria
-*
-* ./build/ComputingMethodsCorCar
-* questo è un normale comando che dice di far partire l'eseguibile
-*
-*******************************************************************************/
-
-
-
+ * \file main.C
+ * \brief main code
+ *
+ * dalla cartella Y-Dimuon
+ *
+ * cmake -S . -B ./build
+ * questo dice dove buildare il codice da quale sorgente (e crea il makefile)
+ *
+ * make -C build
+ * con questo si compila propriamente e si crea la libreria
+ *
+ * ./build/ComputingMethodsCorCar
+ * questo è un normale comando che dice di far partire l'eseguibile
+ *
+ *******************************************************************************/
 
 #include "df_set.h"
 #include "Cuts.h"
@@ -25,12 +22,30 @@
 #include "fitRoo.h"
 #include "ROOT/RDataFrame.hxx"
 
-
-int main(){
-    ROOT::RDataFrame df=df_set();
-    ROOT::RDataFrame df_cut=Cuts(df);
-    TH1* h=SpectrumPlot(df_cut);
-    //fitp1(h,8.5,11.5);
-    fitRoo(h,0);
+int main(int argc, char *argv[])
+{
+    ROOT::RDataFrame df = df_set();
+    ROOT::RDataFrame df_cut = Cuts(df);
+    TH1 *h = SpectrumPlot(df_cut);
+    // fitp1(h,8.5,11.5);
+    int i = 0;
+    if (argc > 1)
+    {
+        if (strcmp(argv[1], "gaus") == 0)
+        {
+            i = 1;
+        }
+        else if (strcmp(argv[1], "bw") == 0)
+        {
+            i = 0;
+        }
+        else
+        {
+            std::cout << "First argument bust be either gaus or bw" << std::endl;
+            std::cout << "Fitting to bw as default" << std::endl;
+            i = 0;
+        }
+    }
+    fitRoo(h, i);
     return 0;
 }
